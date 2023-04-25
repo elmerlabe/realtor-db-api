8f54986ef337894c44b4ba8e600b535db8da82b7
bowkerdev <dev.bowkerideas@gmail.com>
2023-04-25 23:32:56 +0800

    update dabase uri

--- main.py
+++ main.py
@@ -238,8 +238,8 @@ def addNewAgent(cUser):
 
 
 @app.route("/getRealtors", methods=['POST', 'GET'])
-@token_required
-def getRealtors(cUser):
+#@token_required
+def getRealtors():
     page = request.args.get("page", type=int)
     per_page = request.args.get("per_page", type=int)
     sort = request.args.get("sort")
