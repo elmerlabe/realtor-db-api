231d56bf3fb747fc188ca66eba56d52f12cc6a73
bowkerdev <dev.bowkerideas@gmail.com>
2023-04-25 23:47:31 +0800

    added settings.py -> DB_URI, SECRET'

--- main.py
+++ main.py
@@ -15,13 +15,12 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS 
 from sqlalchemy import or_, and_, desc, asc
+from settings import DB_URI, SECRET
 
 app = Flask(__name__)
-app.secret_key = "1234"
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mzrnhsngpzhail:8fff0c777d10daa3dea6ec53594028daaf11fa66c82bb85a8b429035d74d1eaa@ec2-3-219-19-205.compute-1.amazonaws.com:5432/d2065pa7oj1vo7' # heroku-postgres
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/realtors-db' # local database
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://everreach_staging:zytjiv-peprib-fyvvU5@everreach.nell.sh/realtors-db" # Nell remote server
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:jup#H|0?TKffuD?r@34.135.221.137/realtors-db" # Google App Engine
+app.secret_key = SECRET
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 CORS(app)
 db = SQLAlchemy(app)
 
