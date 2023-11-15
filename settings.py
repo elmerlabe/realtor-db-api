5e4ba14d91cfbe6b596824c61de49013e653a9b8
elmer <elmerlabe@gmail.com>
2023-11-15 22:57:40 +0800

    apply fix for heroku database uri

--- settings.py
+++ settings.py
@@ -5,3 +5,8 @@ load_dotenv()
 SECRET = os.getenv('SECRET')
 DATABASE_URL = os.getenv('DATABASE_URL')
 REDISCLOUD_URL = os.getenv('REDISCLOUD_URL')
+
+#heroku database uri fix
+#https://help.heroku.com/ZKNTJQSK/why-is-sqlalchemy-1-4-x-not-connecting-to-heroku-postgres
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://" ,"postgresql://", 1)
